4d297be4aafb0478ba0b2b0e073571795873035b
Nick Maietta <nick@commnetivity.com>
2021-01-19 16:03:22 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,14 +12,13 @@ clear
 
 echo "Now in another browser tab, visit Caprover to create a new App. In your app settings, set the port to 8080. Add any domains FIRST then enable HTTPS only on those domains. Enable websocket support as well, then click, Save & Update.\n\n"
 echo "Now visit the Deployment tab and scroll to \"Method 3\" section. I have copied the project remote URL to clipboard so you can use CTRL-V to paste this into the Repository URL.\n\n"
-echo "In Caprover, create your new App. Set port to 8080 and add any domain names you wish and then enable HTTPS. Save and update.\n\n"
 git config --get remote.origin.url | clip
 
 read -n 1 -r -s -p $'** Press any key to continue\n'
 clear
 
 echo "Set yoru branch to \"main\". Next, i've already copied the private deploy key to your clipboard. Paste this key into the Deploy Key tab and hit Save & Update. This will generate a new webhook URL."
-cat ./deploy.pub | clip
+cat ./deploy | clip
 
 unlink deploy
 unlink deploy.pub
@@ -31,7 +30,7 @@ echo "Go back to your Gitlab project and find the Webhook section. CTRL-V this a
 read -n 1 -r -s -p $'Press any key to exit\n'
 
 clear
-echo "You can now run your project locally using docker-compose up, make changes and commmit them locally. When you push your commit history, your project will automatically be deployed to caprover as a fresh build. ";
+echo "You can now run your project locally using docker-compose up, make changes and commmit them locally. When you push your commit history, your project will automatically be deployed to caprover as a fresh build, replacing the placeholder page ";
 
 unlink setup.sh
 
